721e1d08076879122f105f3d1ffc8727df1ade73
Ye Seul Kim <yskim940914@gmail.com>
2023-08-16 23:28:56 +0900

    Feat: 장바구니 재사용 컴포넌트 생성

---
+++ src/components/Checkbox/Checkbox.tsx
@@ -0,0 +1,38 @@
+interface Checkbox {
+	isChecked: boolean;
+	onChnage: () => void;
+	children: React.ReactNode;
+}
+
+const Checkbox = ({ children, isChecked, onChnage }: Checkbox) => {
+	return (
+		<label>
+			<input type='checkbox' className='sr_only' onChange={onChnage} />
+			<div>
+				<svg
+					width='24'
+					height='24'
+					viewBox='0 0 24 24'
+					fill='none'
+					xmlns='http://www.w3.org/2000/svg'
+				>
+					<path
+						d='M12 24C18.6274 24 24 18.6274 24 12C24 5.37258 18.6274 0 12 0C5.37258 0 0 5.37258 0 12C0 18.6274 5.37258 24 12 24Z'
+						stroke={isChecked ? '' : '#ddd'}
+						fill={isChecked ? '#5f0080' : '#fff'}
+					></path>
+					<path
+						d='M7 12.6667L10.3846 16L18 8.5'
+						stroke={isChecked ? '#fff' : '#ddd'}
+						strokeWidth='1.5'
+						strokeLinecap='round'
+						strokeLinejoin='round'
+					></path>
+				</svg>
+			</div>
+			{children}
+		</label>
+	);
+};
+
+export default Checkbox;
